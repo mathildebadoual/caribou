892002b74d37c76d8f52e0e235171961f2486522
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-01-03 12:17:46 -0800

    update controllers

--- caribou/controllers.py
+++ caribou/controllers.py
@@ -1,10 +1,7 @@
 """Controllers."""
 
-from numpy.random import multivariate_normal
-from numpy.random import rand
-from scipy.linalg import sqrtm
 import numpy as np
-import scipy
+import scipy.linalg
 import quadprog
 
 
@@ -40,27 +37,72 @@ class TravaccaEtAl2017LocalController(LocalController):
 
     def local_solve(self, globalcontroller_variables):
         mu, nu = globalcontroller_variables
+
         b = self.load_b_matrix()
         dam_predict_price = self.globalcontroller.predict_dam_price()
-        fq = np.array(dam_predict_price - nu, np.dot(b, mu))
-        aeq = 0
+        f = np.array(dam_predict_price - nu, np.dot(b, mu))
 
-# TODO(Mathilde): Create a class load or put the load functions outside of classes
+        h = self.load_hq()
+        a = self.create_new_aq()
+        ae = self.load_aeq()
+        b = self.create_new_bq()
+        be = self.load_beq()
+
+        x_result, f_result = self.solve_with_quadprog(h, f, a, ae, b, be)
+        self.g_result = x_result[:24]
+        self.ev_result = x_result[24:]
+        return x_result, f_result
+
+# TODO(Mathilde): create a class with all the solvers modified like this one
+
+    def solve_with_quadprog(self, h, f, a, b, ae, be):
+        """
+        Solve the following quadratic programm using quadprog:
+
+        minimize
+             (1/2) * x.T * h * x + f.T * x
+
+        subject to
+             a * x <= b
+             ae * x == be
+        """
+        h_qp = h
+        f_qp = -f
+        if ae is not None:
+            a_qp = -np.vstack([ae, a]).T
+            b_qp = -np.hstack([be, b])
+            meq = ae.shape[0]
+        else:
+            a_qp = -a.T
+            b_qp = -b
+            meq = 0
+        return quadprog.solve_qp(h_qp, f_qp, a_qp, b_qp, meq)
+
+
+# TODO(Mathilde): Create a class load or put the load functions outside of classes + permit more than 100 houses by building those matrices in the code
 
     def load_b(self):
         return np.genfromtxt('data/travacca_et_al_2017/b.csv', delimiter=',')
 
     def load_e_max(self):
-        return np.genfromtxt('data/travacca_et_al_2017/dam_e_max.csv', delimiter=',')[self.identity]
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/dam_e_max.csv',
+            delimiter=',')[self.identity]
 
     def load_e_min(self):
-        return np.genfromtxt('data/travacca_et_al_2017/dam_e_min.csv', delimiter=',')[self.identity]
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/dam_e_min.csv',
+            delimiter=',')[self.identity]
 
     def load_ev_max(self):
-        return np.genfromtxt('data/travacca_et_al_2017/dam_ev_max.csv', delimiter=',')[self.identity]
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/dam_EV_max.csv',
+            delimiter=',')[self.identity]
 
     def load_ev_min(self):
-        return np.genfromtxt('data/travacca_et_al_2017/dam_ev_min.csv', delimiter=',')[self.identity]
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/dam_EV_min.csv',
+            delimiter=',')[self.identity]
 
     def load_aeq(self):
         return np.genfromtxt('data/travacca_et_al_2017/aeq.csv', delimiter=',')
@@ -71,25 +113,46 @@ class TravaccaEtAl2017LocalController(LocalController):
     def load_beq(self):
         return np.genfromtxt('data/travacca_et_al_2017/beq.csv', delimiter=',')
 
-    def load_c(self):
-        return np.genfromtxt('data/travacca_et_al_2017/c.csv', delimiter=',')
-
     def load_hq(self):
         return np.genfromtxt('data/travacca_et_al_2017/hq.csv', delimiter=',')
 
     def load_lbq(self):
-        return np.genfromtxt('data/travacca_et_al_2017/lbq.csv', delimiter=',')
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/lbq.csv',
+            delimiter=',')[:, self.identity]
 
     def load_ubq(self):
-        return np.genfromtxt('data/travacca_et_al_2017/ubq.csv', delimiter=',')
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/ubq.csv',
+            delimiter=',')[:, self.identity]
 
     def generate_random_pv_gen(self):
         data_pv_gen = self.globalcontroller.load_pv_gen()
-        return data_pv_gen + data_pv_gen * (rand(self.globalcontroller.time_horizon * 24) - 0.5)
+        return data_pv_gen + data_pv_gen * (
+            np.random.rand(self.globalcontroller.time_horizon * 24) - 0.5)
 
     def generate_random_load(self):
         data_dam_demand = self.globalcontroller.load_dam_demand()
-        return data_dam_demand + data_dam_demand * (rand(self.globalcontroller.time_horizon * 24) - 0.5)
+        return data_dam_demand + data_dam_demand * (
+            np.random.rand(self.globalcontroller.time_horizon * 24) - 0.5)
+
+    def create_bq(self):
+        dam_e_max = self.load_e_max()
+        dam_e_min = self.load_e_min()
+        data_pv_gen = self.generate_random_pv_gen()
+        data_dam_load = self.generate_random_load()
+        return np.concatenate(
+            (dam_e_max, -dam_e_min, data_pv_gen - data_dam_load), axis=0)
+
+    def create_new_aq(self):
+        aq = self.load_aq()
+        return np.concatenate((aq, np.eye(48), -np.eye(48)), axis=0)
+
+    def create_new_bq(self):
+        ubq = self.load_ubq()
+        lbq = self.load_lbq()
+        bq = self.create_bq()
+        return np.concatenate((bq, ubq, -lbq), axis=0)
 
 
 class GlobalController(Controller):
@@ -125,16 +188,23 @@ class TravaccaEtAl2017GlobalController(GlobalController):
         return self.data_main[start:stop:4, 16] / scale_pv
 
     def load_e_max_agg(self):
-        return np.genfromtxt('data/travacca_et_al_2017/dam_e_max_agg.csv', delimiter=',')
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/dam_e_max_agg.csv', delimiter=',')
 
     def load_e_min_agg(self):
-        return np.genfromtxt('data/travacca_et_al_2017/dam_e_min_agg.csv', delimiter=',')
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/dam_e_min_agg.csv', delimiter=',')
 
     def load_ev_max_agg(self):
-        return np.genfromtxt('data/travacca_et_al_2017/dam_ev_max_agg.csv', delimiter=',')
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/dam_EV_max_agg.csv', delimiter=',')
 
     def load_ev_min_agg(self):
-        return np.genfromtxt('data/travacca_et_al_2017/dam_ev_min_agg.csv', delimiter=',')
+        return np.genfromtxt(
+            'data/travacca_et_al_2017/dam_EV_min_agg.csv', delimiter=',')
+
+    def load_c(self):
+        return np.genfromtxt('data/travacca_et_al_2017/c.csv', delimiter=',')
 
     def load_dam_price(self):
         start = self.start_day * 4 * 24
@@ -157,28 +227,45 @@ class TravaccaEtAl2017GlobalController(GlobalController):
         dam_price = self.load_dam_price()
         dam_predict_price = np.zeros((24, self.time_horizon))
         for day in range(self.time_horizon):
-            product_matrix = np.reshape(np.dot(
-                    sqrtm(cov_dam_price),
-                    multivariate_normal(np.zeros((24,)), np.eye(24))), (24, 1))
-            matrix_dam_price = np.reshape(dam_price[24 * (
-                day - 1):24 * (day + 1)], (24, 1))
-            dam_predict_price[24 * day:24 * (day + 1), :] = matrix_dam_price + product_matrix
+            product_matrix = np.reshape(
+                np.dot(
+                    scipy.linalg.sqrtm(cov_dam_price),
+                    np.random.multivariate_normal(
+                        np.zeros((24, )), np.eye(24))), (24, 1))
+            matrix_dam_price = np.reshape(
+                dam_price[24 * (day - 1):24 * (day + 1)], (24, 1))
+            dam_predict_price[24 * day:24 * (
+                day + 1), :] = matrix_dam_price + product_matrix
         return dam_predict_price
 
+    def global_solve(self, num_iter=50, gamma=0.00001, alpha=1):
+        mu, nu, g_result, ev_result, local_optimum_cost, total_cost = self.initialize_gradient_accent(
+            self, num_iter)
+        for i in range(num_iter):
+            g_result, ev_result, local_optimum_cost = self.next_step_gradiant_accent(
+                mu, nu, g_result, ev_result, local_optimum_cost)
+            total_cost[i] = self.compute_total_cost(mu, nu, alpha, local_optimum_cost)
 
-    def global_solve(self, num_iter=50, gamma=0.00001):
-        mu, nu, ev_sol, g_sol, local_optimal_cost = self.initialize_gradient_accent(
-            self)
-        for _ in range(num_iter):
-            mu, nu, ev_sol, g_sol, local_optimal_cost = self.next_step_gradiant_accent(
-                mu, nu)
+    def compute_total_cost(self, mu, nu, alpha, local_optimum_cost):
+        cov_dam_price = self.load_cov_dam_price()
+        c = self.load_c()
+        return -1 / (4 * alpha) * np.dot(
+            nu.T, np.dot(np.linalg.inv(cov_dam_price), nu)) + np.dot(
+                c.T, mu) + np.sum(local_optimum_cost)
 
-    def initialize_gradient_accent(self):
+    def initialize_gradient_accent(self, num_iter):
         size = len(self.list_localcontrollers)
-        return 0, 0, np.zeros((24, size)), np.zeros((24, size)), np.zeros(
-            (size, 1))
+        return np.zeros((96, 1)), np.zeros((24, 1)), np.zeros(
+            (24, size)), np.zeros((24, size)), np.zeros((size, 1)), np.zeros(
+                (num_iter, 1))
 
-    def next_step_gradient_accent(self, mu, nu):
+    def next_step_gradient_accent(self, mu, nu, g_result, ev_result,
+                                  local_optimum_cost):
         globalcontroller_variables = (mu, nu)
-        for localcontroller in self.list_localcontrollers:
-            x_result, f_result = localcontroller.run_local_optim(globalcontroller_variables)
+        for i, localcontroller in enumerate(self.list_localcontrollers):
+            x_result, f_result = localcontroller.run_local_optim(
+                globalcontroller_variables)
+            g_result[:, i] = x_result[:24]
+            ev_result[:, i] = x_result[24:]
+            local_optimum_cost[i, 0] = f_result
+        return g_result, ev_result, local_optimum_cost
--- tests/controllers_test.py
+++ tests/controllers_test.py
@@ -1,8 +1,6 @@
 import caribou.controllers as controllers
 import caribou.agentgroups as agentgroups
-import caribou.agents as agents
-import caribou.eventhandlers as eventhandlers
-import caribou.timer as timer
+import numpy as np
 import unittest
 
 
@@ -26,21 +24,29 @@ class TestLoadDataTravaccaEtAl2017LocalController(unittest.TestCase):
     def test_generate_random_load(self):
         self.assertEqual(self.localcontroller.generate_random_load().shape, (24,))
 
+    def test_create_new_aq(self):
+        self.assertEqual(self.localcontroller.create_new_aq().shape, (168, 48))
+
+    def test_create_bq(self):
+        self.assertEqual(self.localcontroller.create_bq().shape, (72,))
+
+    def test_create_new_bq(self):
+        self.assertEqual(self.localcontroller.create_new_bq().shape, (168,))
+
     def test_load_ev(self):
         self.assertEqual(self.localcontroller.load_e_max().shape, (24,))
         self.assertEqual(self.localcontroller.load_e_min().shape, (24,))
         self.assertEqual(self.localcontroller.load_ev_max().shape, (24,))
-        self.assertEqual(self.localcontroller.load_e_min().shape, (24,))
+        self.assertEqual(self.localcontroller.load_ev_min().shape, (24,))
 
     def test_load_matrix_for_optim(self):
         self.assertEqual(self.localcontroller.load_b().shape, (24, 96))
         self.assertEqual(self.localcontroller.load_aeq().shape, (48,))
         self.assertEqual(self.localcontroller.load_aq().shape, (72, 48))
         self.assertEqual(self.localcontroller.load_beq().shape, ())
-        self.assertEqual(self.localcontroller.load_c().shape, (96,))
         self.assertEqual(self.localcontroller.load_hq().shape, (48, 48))
-        self.assertEqual(self.localcontroller.load_lbq().shape, (48, 100))
-        self.assertEqual(self.localcontroller.load_ubq().shape, (48, 100))
+        self.assertEqual(self.localcontroller.load_lbq().shape, (48,))
+        self.assertEqual(self.localcontroller.load_ubq().shape, (48,))
 
 
 class TestConstructionGlobalController(unittest.TestCase):
@@ -60,6 +66,18 @@ class TestLoadDataTravaccaEtAl2017GlobalController(unittest.TestCase):
 
     def test_predict_price(self):
         self.assertEqual(self.globalcontroller.predict_dam_price().shape, (24, 1))
+
+
+class TestRunGradientAccentTravaccaEtAl2017GlobaController(unittest.TestCase):
+    def setUp(self):
+        self.globalcontroller = controllers.TravaccaEtAl2017GlobalController()
+
+    def test_compute_total_cost(self):
+        mu = np.zeros((96, 1))
+        nu = np.zeros((24, 1))
+        alpha = 1
+        local_optimum_cost = np.ones((50, 1))
+        self.assertEqual(self.globalcontroller.compute_total_cost(mu, nu, alpha, local_optimum_cost).shape, (1, 1))
 
 
 if __name__ == '__main__':
